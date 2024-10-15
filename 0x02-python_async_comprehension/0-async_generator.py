1be721d812ad45ad981a3c1c9d5d2ad9faeb5de8
AliTahaa <ali.mohamed.taha.h@gmail.com>
2024-10-15 23:56:03 +0300

    all tasks

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" generator """
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """ yield a random number between 0 and 10 """
+    for _ in range(0, 10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
